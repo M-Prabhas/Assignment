c7a7357e1235ba35473e2c80aa40a6c51704c1a0
Prabhas-M <prabhasmuta99@gmail.com>
2023-06-08 18:48:25 +0530

    commit into prabhas branch

---
+++ index.js
@@ -0,0 +1,75 @@
+const comps = document.getElementsByClassName('drag');
+const dropper = document.getElementsByClassName('dropper')[0];
+console.log("hello");
+// for reverting the containers;
+const buton=document.querySelector('button')
+const container1 = document.querySelector('.container1');
+
+// Store the initial HTML content of the containers
+const initialHTML1 = container1.innerHTML;
+const initialHTML2 = dropper.innerHTML;
+
+//for the items in first container.
+for (child of comps) {
+  child.addEventListener('dragstart', (e) => {
+    console.log("dragstart has been triggered");
+    e.dataTransfer.setData('text/plain', child.src);
+    e.target.className += ' hold'
+    setTimeout(()=>{
+        const newdivElement = document.createElement('div');
+        newdivElement.className='hide';
+        e.target.parentNode.replaceChild(newdivElement, e.target);
+    },0);
+});
+    child.addEventListener('dragend', (e) => {
+        console.log("dragend has been triggered");
+        console.log(e.target);  
+});
+}
+
+
+// for the message section.
+const msg=document.getElementsByClassName('message');
+
+
+dropper.addEventListener('dragover',(e)=>{
+    e.preventDefault();
+})
+
+dropper.addEventListener('dragenter',(e)=>{
+
+});
+
+dropper.addEventListener('dragleave',(e)=>{
+
+});
+
+
+// the message for dropper being filled.
+dropper.addEventListener('drop', (event) => {
+    // Handling the dropped data
+    event.preventDefault();
+    msg.innerHTML="the Item has been dropped.";
+    msg.className='show';
+         // Get the dragged data (in this case, the image source)
+  const imageData = event.dataTransfer.getData('text/plain');
+
+  // Create a new image element
+  const newImageElement = document.createElement('img');
+  newImageElement.src = imageData;
+
+  // Append the new image element to the dropper element
+  dropperElement.appendChild(newImageElement);
+    setTimeout(()=>{  
+      msg.className='message';
+      msg.innerHTML='';
+    },2000);
+  });
+
+// Reset the containers to their initial state when the reset button is clicked
+buton.addEventListener('click', () => {
+  container1.innerHTML = initialHTML1;
+  dropper.innerHTML = initialHTML2;
+})
+
+  
